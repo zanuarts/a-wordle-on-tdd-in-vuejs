f4b680ec4f6c2595ef82d2e86fdafc64c7127f01
Zanuar <zanuar.er@gmail.com>
2024-01-28 16:58:44 +0700

    refactor

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -3,18 +3,20 @@ import WordleBoard from "../WordleBoard.vue";
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from "@/settings";
 
 describe("WordleBoard", () => {
+  const wordOfTheDay = "TESTS"
+
   test("When the user make a guess that matches the word of the day, then a victory message appears", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay} });
 
     const guessInput = wrapper.find("input[type=text]");
-    await guessInput.setValue("TESTS");
+    await guessInput.setValue(wordOfTheDay);
     await guessInput.trigger("keydown.enter");
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
   });
 
   test("If the user makes a guess that is incorrect, then a defeat message appears", async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } });
 
     const guessInput = wrapper.find("input[type=text]");
     await guessInput.setValue("WRONG");
