527ad51802cb882253b2a1100ccb426e1ebbe185
Zanuar <zanuar.er@gmail.com>
2024-01-28 16:56:17 +0700

    create defeat message test

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -1,18 +1,28 @@
-import { mount } from '@vue/test-utils'
-import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from '@/settings'
+import { mount } from "@vue/test-utils";
+import WordleBoard from "../WordleBoard.vue";
+import { VICTORY_MESSAGE } from "@/settings";
 
-describe('WordleBoard', () => {
-  test('When the user make a guess that matches the word of the day, then a victory message appears', async()=>{
-    const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+describe("WordleBoard", () => {
+  test("When the user make a guess that matches the word of the day, then a victory message appears", async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
 
-    const guessInput = wrapper.find("input[type=text]")
-    await guessInput.setValue("TESTS")
-    await guessInput.trigger("keydown.enter")
+    const guessInput = wrapper.find("input[type=text]");
+    await guessInput.setValue("TESTS");
+    await guessInput.trigger("keydown.enter");
 
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
-  })
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE);
+  });
 
-  test.todo("If the user makes a guess that is incorrect, then a defeat message appears")
-  test.todo("If the user has not yet made a guess, then no end-of-game message appears")
-})
+  test("If the user makes a guess that is incorrect, then a defeat message appears", async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" } });
+
+    const guessInput = wrapper.find("input[type=text]");
+    await guessInput.setValue("WRONG");
+    await guessInput.trigger("keydown.enter");
+
+    expect(wrapper.text()).toContain("Better luck next time!");
+  });
+  test.todo(
+    "If the user has not yet made a guess, then no end-of-game message appears"
+  );
+});
