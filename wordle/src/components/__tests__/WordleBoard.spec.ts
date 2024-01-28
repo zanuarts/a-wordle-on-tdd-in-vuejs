ae1bb42441ec331defe397d95211b713564ea1f4
Zanuar <zanuar.er@gmail.com>
2024-01-28 16:32:07 +0700

    remove clutter

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -3,15 +3,12 @@ import WordleBoard from '../WordleBoard.vue'
 
 describe('WordleBoard', () => {
   it('a victory message appears when the user make a guess that matches the word of the day', async()=>{
-    // Arrange
     const wrapper = mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
 
-    // Act
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
 
-    // Assert
     expect(wrapper.text()).toContain("You won!")
   })
 })
