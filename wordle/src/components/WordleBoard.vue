0de2c06509aeda1c13e92a310c4947106be6eb20
Zanuar <zanuar.er@gmail.com>
2024-02-03 17:58:48 +0700

    Enforce that the word of the day must have 5 characters

--- wordle/src/components/WordleBoard.vue
+++ wordle/src/components/WordleBoard.vue
@@ -2,15 +2,25 @@
 import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from "@/settings";
 import { ref } from "vue";
 
-defineProps<{
-  wordOfTheDay: string;
-}>();
+defineProps({
+  wordOfTheDay: {
+    type: String,
+    validator: (wordGiven: string) => wordGiven.length == 5,
+  },
+});
 
-const guessInProgress = ref("")
-const guessSubmitted = ref("")
+const guessInProgress = ref("");
+const guessSubmitted = ref("");
 </script>
 
 <template>
-  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted=guessInProgress"/>
-  <p v-if="guessSubmitted.length > 0" v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"/>
+  <input
+    type="text"
+    v-model="guessInProgress"
+    @keydown.enter="guessSubmitted = guessInProgress"
+  />
+  <p
+    v-if="guessSubmitted.length > 0"
+    v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+  />
 </template>
