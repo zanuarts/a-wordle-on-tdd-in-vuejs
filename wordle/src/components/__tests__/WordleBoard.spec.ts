e208bbe53381d04db3ba935cf0c72ff98c80e712
Zanuar <zanuar.er@gmail.com>
2024-02-03 19:18:09 +0700

    Ensure no warnings are rendered when a proper word of the day is provided

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -56,4 +56,12 @@ describe("WordleBoard", () => {
 
     expect(console.warn).toHaveBeenCalled;
   });
+
+  test("No warning is emitted if the word of the day provided is a real uppercase English word with 5 characters", async()=>{
+    console.warn = vi.fn();
+
+    mount(WordleBoard, { props: { wordOfTheDay: "TEST" } });
+
+    expect(console.warn).not.toHaveBeenCalled;
+  })
 });
