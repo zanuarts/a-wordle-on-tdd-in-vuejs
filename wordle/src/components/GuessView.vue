e1ef932605183f689cab4755a51f285df462ed41
Zanuar <zanuar.er@gmail.com>
2024-02-04 17:28:04 +0700

    Display unknown feedback upon successful guess submission

--- wordle/src/components/GuessView.vue
+++ wordle/src/components/GuessView.vue
@@ -12,6 +12,7 @@ withDefaults(defineProps<{ guess: string; shouldFlip?: boolean }>(), {
       v-for="(letter, index) in guess.padEnd(WORD_SIZE, ' ')"
       :key="`${letter}-${index}`"
       :data-letter="letter"
+      :data-letter-feedback="shouldFlip ? 'unknown' : null"
       :class="{ 'with-flips': shouldFlip }"
       class="letter"
       v-text="letter"
