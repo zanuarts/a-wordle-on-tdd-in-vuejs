79d286b4496f7d6774c937f2b6914cac1dea9929
Zanuar <zanuar.er@gmail.com>
2024-02-04 17:20:30 +0700

    Add card flipping animation when a guess is submitted

--- wordle/src/components/GuessView.vue
+++ wordle/src/components/GuessView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { WORD_SIZE } from "@/settings";
 
-defineProps<{ guess: string }>();
+withDefaults(defineProps<{ guess: string; shouldFlip?: boolean }>(), {
+  shouldFlip: false,
+});
 </script>
 
 <template>
@@ -10,13 +12,14 @@ defineProps<{ guess: string }>();
       v-for="(letter, index) in guess.padEnd(WORD_SIZE, ' ')"
       :key="`${letter}-${index}`"
       :data-letter="letter"
+      :class="{ 'with-flips': shouldFlip }"
       class="letter"
       v-text="letter"
     />
   </ul>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 ul {
   margin: 0;
   padding: 0;
@@ -28,7 +31,9 @@ ul {
   gap: 0.25rem;
 }
 .letter {
-  background-color: white;
+  --front-color: hsl(0, 0%, 99%);
+  --back-color: hsl(0, 0%, 70%);
+  background-color: var(--front-color);
   border: 1px solid hsl(0, 0%, 70%);
   width: 5rem;
   height: 5rem;
@@ -47,6 +52,31 @@ li:not([data-letter=" "]) {
   }
   50% {
     transform: scale(1.4);
+  }
+}
+
+$maxWordSize: 5;
+@for $i from 1 through $maxWordSize {
+  .with-flips:nth-of-type(#{$i}) {
+    animation: flip-card 300ms forwards;
+    animation-delay: #{250 * $i}ms;
+  }
+}
+@keyframes flip-card {
+  0% {
+    transform: rotateY(0);
+    background-color: var(--front-color);
+  }
+  49% {
+    background-color: var(--front-color);
+  }
+  50% {
+    transform: rotateY(-90deg);
+    background-color: var(--back-color);
+  }
+  100% {
+    transform: rotateY(0);
+    background-color: var(--back-color);
   }
 }
 </style>
--- wordle/src/components/WordleBoard.vue
+++ wordle/src/components/WordleBoard.vue
@@ -32,7 +32,7 @@ const countOfEmptyGuesses = computed(() => {
   <main>
     <ul>
       <li v-for="(guess, index) in guessesSubmitted" :key="`${index}-${guess}`">
-        <guess-view :guess="guess" />
+        <guess-view :guess="guess" should-flip />
       </li>
       <li>
         <guess-input
