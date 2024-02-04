37a2edc9d0be3a3237c7c6e40dff981c037baef3
Zanuar <zanuar.er@gmail.com>
2024-02-04 14:52:45 +0700

    Make GuessInput prettier

--- wordle/src/components/GuessInput.vue
+++ wordle/src/components/GuessInput.vue
@@ -32,6 +32,16 @@ function onSubmit() {
 </script>
 
 <template>
+  <ul class="word">
+    <li
+      v-for="(letter, index) in formattedGuessInProgress.padEnd(WORD_SIZE, ' ')"
+      :key="`${letter}-${index}`"
+      :data-letter="letter"
+      class="letter"
+      v-text="letter"
+    />
+  </ul>
+
   <input
     type="text"
     v-model="formattedGuessInProgress"
@@ -41,3 +51,38 @@ function onSubmit() {
     @blur="({target}) => (target as HTMLInputElement).focus()"
   />
 </template>
+
+<style scoped>
+input {
+  position: absolute;
+  opacity: 0;
+}
+.word {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  gap: 0.25rem;
+}
+.letter {
+  background-color: white;
+  border: 1px solid hsl(0, 0%, 70%);
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bolder;
+}
+li:not([data-letter=" "]) {
+  animation: pop 100ms;
+}
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.4);
+  }
+}
+</style>
