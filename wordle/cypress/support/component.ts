e12e277fe65a25e33d9f6afbc64e75bfdd49f894
Zanuar <zanuar.er@gmail.com>
2024-02-10 14:00:01 +0700

    Setup Visual Regression testing for WordleBoard

--- wordle/cypress/support/component.ts
+++ wordle/cypress/support/component.ts
@@ -14,25 +14,27 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import "./commands"
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import {mount} from "cypress/vue"
+import { mount } from "cypress/vue";
+import { addMatchImageSnapshotCommand } from "@simonsmith/cypress-image-snapshot/command";
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 declare global {
-    namespace Cypress {
-        interface Chainable {
-            mount: typeof mount
-        }
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount;
     }
+  }
 }
 
-Cypress.Commands.add("mount", mount)
+Cypress.Commands.add("mount", mount);
+addMatchImageSnapshotCommand();
 
 // Example use:
 // cy.mount(MyComponent)
